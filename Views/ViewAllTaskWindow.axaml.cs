e6321e4e2fd0f4bb1613972604227bbdc1d1d2a5
HillyDaan <daan.hillebrand01@gmail.com>
2024-11-21 12:03:45 +0100

    Fix: bug in namespace
    Namespace bug in creating a new view.
    Also fixes some unfortunate testing comments

--- Views/ViewAllTaskWindow.axaml.cs
+++ Views/ViewAllTaskWindow.axaml.cs
@@ -2,12 +2,14 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace ToDolistVersion2;
-
-public partial class ViewAllTaskWindow : Window
-{
-    public ViewAllTaskWindow()
+namespace ToDolistVersion2.Views
+{    
+    public partial class ViewAllTaskWindow : Window
     {
-        InitializeComponent();
+        public ViewAllTaskWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
+
